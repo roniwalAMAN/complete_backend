7cbe6987a2ba71da84ee0e55c2fa5a4f15d6a6fc
Aman Roniwal <amanroniwal@Amans-MacBook-Air-2.local>
2025-08-16 23:08:07 +0530

    utiles

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,6 @@ const connectDB = async ()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-        
     } catch (error) {
         console.log("MONGODB connection failed", error);
         process.exit(1);
