1b46726fb6bdf419ca8c9ebaa178663be087de77
Aman Roniwal <amanroniwal@Amans-MacBook-Air-2.local>
2025-08-21 23:25:25 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-const app = express()
+
+
+const app = express();
+
 
 
 
@@ -15,4 +18,15 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: '16kb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes
+import userRouter from './routes/user.routes.js'
+
+
+
+//routes decleration 
+app.use("/api/v1/users", userRouter)
+// http://localhost:8000/api/v1/users/register
+
+
 export { app }
